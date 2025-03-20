7d4e731a861e632ebd506a4ebb315b3d6affc541
Nadya-pratikumadp <nadyaputri200706@gmail.com>
2025-03-20 17:52:16 +0700

    Add files via upload

---
+++ tupas modul 3.py
@@ -0,0 +1,28 @@
+print ("soal:", "\n")
+print ("Misalkan N(t) adalah variable acak poissison dengan parameter lambda_t>0")
+print ("             P(N(t) = n) e**(-lambda_t)*(lambda_t)**N/(n!)              ")
+print ("Buatlah suatu program untuk menghitung P(N(t) = n), inputkan nilai terlebih dahulu.            ","\n")
+lambda_t = int (input("masukkan nilai lambda t         : "))
+m        = int (input("input nilai m                   : "))
+e = 2.71828
+print ("\n")
+for b in range (0,m+1):
+    faktorial_b = 1
+    for i in range (1,b+1):
+        faktorial_b *= i
+    n=(e**(-lambda_t)) * ((lambda_t)**b)/faktorial_b 
+    
+    print(f"maka nilai P(N(t) ke -{b} adalah :{n} {m} faktiorial adalah         :{faktorial_b}")
+
+
+
+
+
+
+
+
+
+
+
+    
+
